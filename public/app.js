6fab053e0bf3707d590381e79cf230a64dfd00e0
Shrey Keny <46216791+shreykeny@users.noreply.github.com>
2021-09-24 16:18:33 +0530

    added dynamic code to events page

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
+
 //  dark mode
 console.log("Made with love by Computer Department :)");
 
 function myFunction() {
-const html =  document.querySelector("html");
-html.classList.toggle("dark");
+  const html = document.querySelector("html");
+  html.classList.toggle("dark");
 }
 
 myFunction();
@@ -12,75 +13,73 @@ myFunction();
 
 // back to top
 
-            //Get the button
-            var mybutton = document.getElementById("myBtn");
-            window.onscroll = function() {scrollFunction()};
-            
-            function scrollFunction() {
-              if (document.body.scrollTop > 300 || document.documentElement.scrollTop >300) {
-                mybutton.style.display = "block";
-              } else {
-                mybutton.style.display = "none";
-              }
-            }
-            
-            // When the user clicks on the button, scroll to the top of the document
-            function topFunction() {
-             
-              $("html, body").animate(
-                { scrollTop: "0" }, 800);
-            }
-          
-             
-           
+//Get the button
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+
+  $("html, body").animate(
+    { scrollTop: "0" }, 800);
+}
+
+
+
 // back to top ENDDD
 
 // SMOOTH SCROLLING
-$('.smooth').on('click',function(event)
-{if(this.hash !==''){
-  event.preventDefault();
-  const hash =this.hash;
-  $('html,body').animate(
-    {
-      scrollTop:$(hash).offset().top   },800
-  
-  );
-}});
+$('.smooth').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+    $('html,body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      }, 800
+
+    );
+  }
+});
 //ENDD
 
 // FILTER CODE
-function  filterObject(c)
-{  
-  var x ,i;
-  x= document.getElementsByClassName("cardE");
-  if(c=="all")
-  { c="";  
-} else {
-  
-}
- 
-  for(i=0;i<x.length;i++)
-  {
-    removeClass(x[i],"block");
-    removeClass(x[i],"hidden");
-    if(x[i].className.indexOf(c)>-1) 
-    { addClass(x[i],"block"); 
-         
+function filterObject(c) {
+  var x, i;
+  x = document.getElementsByClassName("cardE");
+  if (c == "all") {
+    c = "";
   } else {
-    addClass(x[i],"hidden");
+
   }
+
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "block");
+    removeClass(x[i], "hidden");
+    if (x[i].className.indexOf(c) > -1) {
+      addClass(x[i], "block");
+
+    } else {
+      addClass(x[i], "hidden");
+    }
   }
 }
-function addClass(element,name)
-{
-  var i,arr1,arr2;
-  arr1= element.className.split(" ");
-  arr2=name.split(" ");
-  for(i=0 ;i<arr2.length;i++)
-  { if(arr1.indexOf(arr2[i])==-1)
-   {
-     element.className += " " + arr2[i];
-   }
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
   }
 }
 function removeClass(element, name) {
@@ -89,7 +88,7 @@ function removeClass(element, name) {
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
   element.className = arr1.join(" ");
@@ -98,14 +97,14 @@ function removeClass(element, name) {
 var accItem = document.getElementsByClassName('accordionItem');
 var accHD = document.getElementsByClassName('accordionItemHeading');
 for (i = 0; i < accHD.length; i++) {
-    accHD[i].addEventListener('click', toggleItem, false);
+  accHD[i].addEventListener('click', toggleItem, false);
 }
 function toggleItem() {
-    var itemClass = this.parentNode.className;
-    for (i = 0; i < accItem.length; i++) {
-        accItem[i].className = 'accordionItem close';
-    }
-    if (itemClass == 'accordionItem close') {
-        this.parentNode.className = 'accordionItem open';
-    }
+  var itemClass = this.parentNode.className;
+  for (i = 0; i < accItem.length; i++) {
+    accItem[i].className = 'accordionItem close';
+  }
+  if (itemClass == 'accordionItem close') {
+    this.parentNode.className = 'accordionItem open';
+  }
 }
