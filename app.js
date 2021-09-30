2549321b37519bf653814642f06a7223370e44d2
Shrey Keny <46216791+shreykeny@users.noreply.github.com>
2021-09-30 19:44:29 +0530

    added new Codeathon poster

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 //  dark mode
 console.log("Made with love by IT and Computer Department :)");
-console.log(" Have a Good day  :)");
-console.log(" Biryani is love :)");
-console.log("Hello curious coder, Abdullah has a suprise for you! Find the bug and the treat is yours!");
+// console.log(" Have a Good day  :)");
+// console.log(" Biryani is love :)");
+// console.log("Hello curious coder, Abdullah has a suprise for you! Find the bug and the treat is yours!");
 
 function darkFunction() {
   const html = document.querySelector("html");
@@ -63,40 +63,40 @@ $('.smooth').on('click', function (event) {
 
 // FILTER CODE
 $(document).ready(function () {
-  var dept=sessionStorage.getItem('filter') === null? null:$("#"+sessionStorage.getItem('filter')).attr('id')
-  if(dept=== "ALL")
-   filterAll()
- else if(dept!=null)
-   filterDepartment(dept)
-   
- $('.category_item').click(function () {
-   var category = $(this).attr('id')
-   console.log(category)
-   if (category == 'ALL') {
+  var dept = sessionStorage.getItem('filter') === null ? null : $("#" + sessionStorage.getItem('filter')).attr('id')
+  if (dept === "ALL")
     filterAll()
-   } else {
-    filterDepartment(category)
-   }
-   //adding filters to session storage
-   sessionStorage.setItem('filter', category);
- })
-})
-function filterAll(){
- $('.event_item').addClass('hide');
- $('.event_item').addClass('show');
+  else if (dept != null)
+    filterDepartment(dept)
 
- $('.event_item').removeClass('hide');
- setTimeout(function () {
-   $('.event_item').removeClass('show');
- }, 350);
+  $('.category_item').click(function () {
+    var category = $(this).attr('id')
+    console.log(category)
+    if (category == 'ALL') {
+      filterAll()
+    } else {
+      filterDepartment(category)
+    }
+    //adding filters to session storage
+    sessionStorage.setItem('filter', category);
+  })
+})
+function filterAll() {
+  $('.event_item').addClass('hide');
+  $('.event_item').addClass('show');
+
+  $('.event_item').removeClass('hide');
+  setTimeout(function () {
+    $('.event_item').removeClass('show');
+  }, 350);
 }
-function filterDepartment(dept){
- $('.event_item').addClass('hide');
- $('.' + dept).addClass('show');
- $('.' + dept).removeClass('hide');
- setTimeout(function () {
-   $('.' + dept).removeClass('show');
- }, 350);
+function filterDepartment(dept) {
+  $('.event_item').addClass('hide');
+  $('.' + dept).addClass('show');
+  $('.' + dept).removeClass('hide');
+  setTimeout(function () {
+    $('.' + dept).removeClass('show');
+  }, 350);
 }
 
 // accordion
